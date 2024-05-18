cfc2747609568cf3e5fc17b556a569897a775cb0
Sarath Krishnan P V <142596951+PVSarathKrishnan@users.noreply.github.com>
2024-05-18 16:17:42 +0530

    Create file

---
+++ 1827-MinimumOperationstoMaketheArrayIncreasing/1827-MinimumOperationstoMaketheArrayIncreasing.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minOperations(int[] nums) {
+        int result = 0;
+        for(int i = 1; i < nums.length; i++) {
+            if(nums[i] <= nums[i-1]) {
+                int increment = nums[i-1] - nums[i] + 1;
+                nums[i] += increment;
+                result += increment;
+            }
+        }
+        return result;
+    }
+}
+
+[
